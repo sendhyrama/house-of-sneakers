990db8325f68d690bde4499ab4f2c30e8f8d2325
Sendhy Ramadhinata <sendhyrama@gmail.com>
2022-07-25 20:21:46 +0700

    add detail produk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parentbox">
+      <FotoProduk/>
+      <ProdukInfo name="Converse CT 70's High" category="CASUAL"/>
     </div>
+  );
+}
+
+function FotoProduk() {
+  return (
+    <div className="foto">
+      <img src="sepatu.png" alt="foto-sepatu"></img>
+    </div>
+
+  );
+}
+
+function ProdukInfo(props) {
+  const {name, category} = props;
+  return (
+      <div className="deskripsi">
+        <p className="cate">{category}</p>
+        <h1 className="title">{name}</h1>
+        <p className="harga">IDR 799.000,00</p>
+        <p className="info">
+        The Chuck Taylor All Star is the most iconic sneaker in the world, recognized for its unmistakable silhouette and cultural authenticity. And like all like the best paradigms, they only get better with time. Find here the classic colors that have donned it's low top canvas upper for generations, all resting on top of a quality vulcanized rubber sole. It is the sneaker you choose to do whatever you want in.
+        </p>
+      </div>
   );
 }
 
